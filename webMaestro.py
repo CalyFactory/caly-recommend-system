5f01c03541f0740b9b967f05f3032a5459d1b57d
yenoss <cpg0504@gmail.com>
2017-06-07 22:59:33 +0900

    refactoring reco modules

---
+++ webMaestro.py
@@ -0,0 +1,205 @@
+
+from common import db_manager
+from common.util import utils
+from extractor.event_extractor import extract_info_from_event
+from reinforce.reinforce import Reinforce
+
+
+class WebMaestro():	
+
+	#실제 유저 이벤트
+	result_events = []
+	#[웹뷰용]형태소분석 된 이후 분석된 이벤트 정보를 웹뷰에 보여주기위한 데이터
+	result_analysis_events_for_web = []
+	#최종 웹에 보여줄 데이터
+	result_final= {}
+	#[웹뷰용]보강된 데이터 넘겨줄 배열
+	result_reinforce_events_for_web = []
+	#실제 넘길데이터
+	result_reinforce_events_real = []
+
+	event = {}
+	reinforce_json = {}
+	extracted_json = {}
+	serMainRegion = {}
+	calendar_hashkey = None
+	calendar_name = None
+	user_event_hashkey = None
+	
+	def __init__(self):
+		pass
+
+	def __get__(self, obj, objtype):
+		return self.val
+
+	def __set__(self, obj, val):
+		self.val = val
+
+	def getUserMainRegion(self):
+		self.userMainRegion = utils.fetch_all_json(
+			db_manager.query(
+					"""
+								SELECT region, count(*) as locationCnt FROM
+								(
+								SELECT ul.region,ul.location_hashkey,ul.priority 
+								FROM EVENT e, USER_EVENT_ANALYSIS ue , USER_EVENT_LOCATION ul ,
+								(
+								# 여기까지 유저 accounthashkey가 같은 calendar 가져오기
+								SELECT calendar_hashkey FROM CALENDAR
+								INNER JOIN(
+								#여기까지 이벤트 해시키로 유저 account_hashkey들 가져오는쿼리
+								SELECT account_hashkey FROM USERACCOUNT
+								WHERE user_hashkey IN (
+								SELECT user_hashkey 
+								FROM EVENT e, CALENDAR c, USERACCOUNT u
+								WHERE e.event_hashkey = %s
+								AND c.calendar_hashkey = e.calendar_hashkey
+								AND u.account_hashkey = c.account_hashkey
+								)	
+								#############
+								) AS u
+								ON u.account_hashkey = CALENDAR.account_hashkey
+								#############
+								) cal
+								WHERE e.calendar_hashkey = cal.calendar_hashkey
+								AND ue.event_hashkey = e.event_hashkey
+								AND ue.location_hashkey = ul.location_hashkey
+								AND ul.priority = 1
+								) AS locations
+								GROUP by region
+								ORDER BY locationCnt DESC
+								LIMIT 3				
+					""",							
+					(self.user_event_hashkey,)
+				)
+			)		
+
+	def makeFinalReuslt(self):
+		self.result_final["originEvents"] = self.result_events
+		self.result_final["analysisEvents"] = self.result_analysis_events_for_web
+		self.result_final["reinforceEvents"] = self.result_reinforce_events_for_web
+		self.result_final["reinforceEventsReco"] = self.result_reinforce_events_real
+		self.result_final["userLocations"] = self.userMainRegion
+
+
+
+
+	def checkRecoStauts(self):
+		reinforce = self.__reinforceFromExtracted()
+		if reinforce.event_reco_result["code"] != 2 :
+			self.result_reinforce_events_for_web.append(
+											{
+												"event_types":self.__pretty_type(self.reinforce_json["event_types"]),
+												"locations":self.__pretty_locations(self.reinforce_json["locations"])														
+											}
+										)
+
+		else:
+			self.result_reinforce_events_for_web.append(
+											{
+												"event_types":"-",
+												"locations":"-"			
+											}
+										)			
+
+	def __reinforceFromExtracted(self):
+		reinforce = Reinforce(self.extracted_json)
+		self.reinforce_json = reinforce.event_reco_result["event_info_data"]					
+		self.result_reinforce_events_real.append(self.reinforce_json)
+		return reinforce
+
+
+
+
+			
+			
+	def append_analaysis_events_for_web(self):
+		self.result_analysis_events_for_web.append(
+														{
+															"event_types":self.__pretty_type(self.extracted_json["event_types"]),
+															"locations":self.__pretty_locations(self.extracted_json["locations"]),
+															"time_set":self.extracted_json["time_set"]	
+														}
+													)
+
+	def extractFromEvent(self):
+
+		if self.event["location"] == None:
+			self.event["location"] = ''
+			
+		print('event_title => ' + self.event["summary"])
+		print('event_start_dt => ' + self.event["start_dt"])
+		print('event_end_dt => ' + self.event["end_dt"])
+		print('event_location => ' + self.event["location"])			
+		self.extracted_json = extract_info_from_event(self.event["event_hashkey"],self.event["summary"],self.event["start_dt"],self.event["end_dt"],self.event["location"])
+
+	def appendEvent(self):
+		self.user_event_hashkey = self.event["event_hashkey"]
+		self.result_events.append(
+						{
+							"calendarName":self.calendar_name,
+							"eventSummary":self.event["summary"],
+							"eventStartDt":self.event["start_dt"],
+							"eventEndDt":self.event["end_dt"],
+							"eventLocation":self.event["location"],
+						}
+					)	
+
+
+	def getEventsList(self,calendar_hashkey):
+		return utils.fetch_all_json(
+			db_manager.query(
+					"""
+					SELECT * FROM EVENT 
+					WHERE calendar_hashkey = %s 
+					ORDER BY start_dt
+					""",							
+					(calendar_hashkey,)
+			)
+		)		
+	def getCalendarList(self,account_hashkey):
+		return utils.fetch_all_json(
+			db_manager.query(
+					"""
+					SELECT * FROM CALENDAR
+					WHERE account_hashkey = %s 
+					
+					""",							
+					(account_hashkey,)
+			)
+		)		
+
+	def __pretty_locations(self,extracted_locations):
+		if extracted_locations == 'None' or extracted_locations == 'Cannot':
+			real_extracted_locations = extracted_locations				
+		else :	
+			real_extracted_locations = ''		
+			for loca in extracted_locations:
+				real_extracted_locations += loca["region"] + " "
+
+
+		return real_extracted_locations
+
+	def __pretty_type(self,extracted_event_types):
+		if extracted_event_types == 'None' :
+			real_extracted_event_types = extracted_event_types	
+		else:
+			real_extracted_event_types = ''
+			for e_type in extracted_event_types:						
+				# print("test"+str(e_type)+" end")
+				type_name = ''
+				if str(e_type["id"]) == 'CPI01':
+					type_name = '지인과의 약속'
+				if str(e_type["id"]) == 'CPI02':
+					type_name = '데이트'	
+				if str(e_type["id"]) == 'CPI03':
+					type_name = '기념일'		
+				if str(e_type["id"]) == 'CPI04':
+					type_name = '뒷풀이'			
+				if str(e_type["id"]) == 'CPI05':
+					type_name = '회의/스터디'										
+				if str(e_type["id"]) == 'CPI06':
+					type_name = '문화생활'		
+				real_extracted_event_types += str(type_name) + " "	
+
+		return real_extracted_event_types
--- webReco.py
+++ webReco.py
@@ -10,6 +10,9 @@ from common import db_manager
 from common.util import utils
 from extractor.event_extractor import extract_info_from_event
 from reinforce.reinforce import Reinforce
+
+import webMaestro
+
 from reco.reco import Reco
 from flask_cors import CORS
 
@@ -37,46 +40,13 @@ def index():
 							"accountHashkey":row["account_hashkey"]
 						}
 					)
-	print(users)
+	# print(users)
 
 
 	return render_template('index.html',
 							users = users)
 
-def prettyLocations(extracted_locations):
-	if extracted_locations == 'None' or extracted_locations == 'Cannot':
-		real_extracted_locations = extracted_locations				
-	else :	
-		real_extracted_locations = ''		
-		for loca in extracted_locations:
-			real_extracted_locations += loca["region"] + " "
 
-
-	return real_extracted_locations
-
-def prettyType(extracted_event_types):
-	if extracted_event_types == 'None' :
-		real_extracted_event_types = extracted_event_types	
-	else:
-		real_extracted_event_types = ''
-		for e_type in extracted_event_types:						
-			# print("test"+str(e_type)+" end")
-			type_name = ''
-			if str(e_type["id"]) == 'CPI01':
-				type_name = '지인과의 약속'
-			if str(e_type["id"]) == 'CPI02':
-				type_name = '데이트'	
-			if str(e_type["id"]) == 'CPI03':
-				type_name = '기념일'		
-			if str(e_type["id"]) == 'CPI04':
-				type_name = '뒷풀이'			
-			if str(e_type["id"]) == 'CPI05':
-				type_name = '회의/스터디'										
-			if str(e_type["id"]) == 'CPI06':
-				type_name = '문화생활'		
-			real_extracted_event_types += str(type_name) + " "	
-
-	return real_extracted_event_types
 
 @app.route("/eventsDetail", methods = ["GET"])
 def eventsDetail(): 
@@ -88,122 +58,61 @@ def eventsDetail():
 
 @app.route("/events", methods = ["GET"])
 def events(): 
-	account_hashkey = request.args['accountHashkey']
-	result_events = []
-	result_analysis_events = []
-	result_sum = {}
-	result_reinforce_events = []
-	result_reinforce_events_real = []
 
-	calendars = utils.fetch_all_json(
-		db_manager.query(
-				"""
-				SELECT * FROM CALENDAR
-				WHERE account_hashkey = %s 
+	account_hashkey = request.args['accountHashkey']
+
+	# #실제 유저 이벤트
+	# result_events = []
+	# #[웹뷰용]형태소분석 된 이후 분석된 이벤트 정보를 웹뷰에 보여주기위한 데이터
+	# result_analysis_events_for_web = []
+	# #최종 웹에 보여줄 데이터
+	# result_final= {}
+	# #[웹뷰용]보강된 데이터 넘겨줄 배열
+	# result_reinforce_events_for_web = []
+	# #실제 넘길데이터
+	# result_reinforce_events_real = []
+
+	# calendars = utils.fetch_all_json(
+	# 	db_manager.query(
+	# 			"""
+	# 			SELECT * FROM CALENDAR
+	# 			WHERE account_hashkey = %s 
 				
-				""",							
-				(account_hashkey,)
-		)
-	)
+	# 			""",							
+	# 			(account_hashkey,)
+	# 	)
+	# )
+	
+	# user_event_hashkey = ''
+	web_maestro = webMaestro.WebMaestro()
+	calendars = web_maestro.getCalendarList(account_hashkey)	
 
 	for calendar in calendars:
-		calendar_hashkey = calendar["calendar_hashkey"]
-		calendar_name = calendar["calendar_name"]
-		events = utils.fetch_all_json(
-			db_manager.query(
-					"""
-					SELECT * FROM EVENT 
-					WHERE calendar_hashkey = %s
-					""",							
-					(calendar_hashkey,)
-			)
-		)
+		web_maestro.calendar_hashkey = calendar["calendar_hashkey"]
+		web_maestro.calendar_name = calendar["calendar_name"]
 		
+		events = web_maestro.getEventsList(web_maestro.calendar_hashkey)
+		# print("events ==> "+str(events))
 		for event in events:
 			if 'summary' in event:
-				# print("events ==>> "+str(event))
-				event_summary = event["summary"]
-				event_start_dt = event["start_dt"]
-				event_end_dt = event["end_dt"]
-				event_location = event["location"]
-				event_hashkey = event["event_hashkey"]
+				print("events ==> "+str(event))
 
-				result_events.append(
-								{
-									"calendarName":calendar_name,
-									"eventSummary":event_summary,
-									"eventStartDt":event_start_dt,
-									"eventEndDt":event_end_dt,
-									"eventLocation":event_location,
-								}
-							)
-				if event_location == None:
-					event_location = ''
-				# print(event_summary)
-				# print(event_start_dt)
-				# print(event_end_dt)
-				# print(event_location)
-				try:
-					extracted_json = extract_info_from_event(event_hashkey,event_summary,event_start_dt,event_end_dt,event_location)
-				except Exception as e:
-					print('error!!>>'+str(e))
+				web_maestro.event = event
+				web_maestro.appendEvent()				
 
-				# print('extraced_json =>'+str(extracted_json))
+				web_maestro.extractFromEvent()
 
-				extracted_locations = extracted_json["locations"]
-				extracted_event_types = extracted_json["event_types"]
-				extracted_time_set = extracted_json["time_set"]
+				print("extcted_json => "+ str(web_maestro.extracted_json))
 
-				
-				real_extracted_locations = prettyLocations(extracted_locations)					
+				web_maestro.append_analaysis_events_for_web()
 
+				web_maestro.checkRecoStauts()
 
-				real_extracted_event_types = prettyType(extracted_event_types)
-
-				result_analysis_events.append(
-												{
-													"event_types":real_extracted_event_types,
-													"locations":real_extracted_locations,
-													"time_set":extracted_time_set
-												}
-											)
-				reinforce = Reinforce(extracted_json)
-				# print("recoResult = > "+ str(reinforce.event_reco_result))
-				reinforce_json = reinforce.event_reco_result["event_info_data"]					
-				result_reinforce_events_real.append(reinforce_json)
-
-				if reinforce.event_reco_result["code"] != 2 :
-
-					reinforce_locations = reinforce_json["locations"]
-					reinforce_event_types = reinforce_json["event_types"]
-
-					real_reinforce_locations = prettyLocations(reinforce_locations)					
-					real_reinforce_event_types = prettyType(reinforce_event_types)	
-					# print(real_reinforce_locations)
-					# print(real_reinforce_event_types)
-					result_reinforce_events.append(
-													{
-														"event_types":real_reinforce_event_types,
-														"locations":real_reinforce_locations													
-													}
-												)
-
-				else:
-					result_reinforce_events.append(
-													{
-														"event_types":"-",
-														"locations":"-"			
-													}
-												)	
-
-	result_sum["originEvents"] = result_events
-	result_sum["analysisEvents"] = result_analysis_events
-	result_sum["reinforceEvents"] = result_reinforce_events
-	result_sum["reinforceEventsReco"] = result_reinforce_events_real
-
-
-
-	return json.dumps(result_sum)
+	web_maestro.getUserMainRegion()
+	web_maestro.makeFinalReuslt()
+	# print("final web Return result"+str(web_maestro.result_final))
+	return json.dumps(web_maestro.result_final)
+	# return 'hi'
 
 
 
