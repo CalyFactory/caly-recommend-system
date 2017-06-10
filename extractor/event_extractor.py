02dab3c43dc1b3f338d60bcce5da694a7ab33a5a
refgjin <refgjin@gmail.com>
2017-06-11 02:35:25 +0900

    Comment code-review & Update Testcase

--- extractor/event_extractor.py
+++ extractor/event_extractor.py
@@ -49,21 +49,23 @@ def load_subway_dict():
 
 	return [region_dict, univ_dict]
 
-def extract_info_from_event(event_hashkey,sentence,start_dt, end_dt, location):
-	full_sentence=location+' '+sentence
+def extract_info_from_event(event_hashkey,summary,start_dt, end_dt, location):
+	full_sentence=location+' '+summary
 	region_collect_dict, univ_collect_dict = load_subway_dict()
-	
+
 	location_count=0
 	location_list=[]
+	# 2017-07-04 12:00:00
 	splited_start_dt=str(start_dt).split(' ')
 	splited_end_dt=str(end_dt).split(' ')
-
+	# 12:00:00
 	time_zone_dict={
 		'event_start':'',
 		'event_end':'',
 		'event_start':splited_start_dt[1][:5],
 		'event_end':splited_end_dt[1][:5]
 	}
+	# 12:00
 	purpose_count = {
 		'CPI01':0,
 		'CPI02':0,
@@ -87,41 +89,49 @@ def extract_info_from_event(event_hashkey,sentence,start_dt, end_dt, location):
 	
 		t.parse(full_sentence)
 		time = -1
+		number = -1
 		total_purpose_count=0
 		m = t.parseToNode(full_sentence)
-		while m:
+		
+		while m: 
 			### Grep time-zone : ical / line_plus@naver.com
-			if m.feature.find('SN') > -1 and m.surface.isdigit():
-				time=m.surface
-			elif m.feature.find('NNBC') > -1 and m.feature.find('시') > -1 and time is not -1:
-				exist_time=True
-				end_time = int(time)+1
+			if (m.surface.find('아침') > -1) or (m.surface.find('브런치') > -1) or (m.surface.find('점심') > -1) or (m.surface.find('저녁') > -1) or (m.surface.find('밤') > -1):
+				time=standard_time_scope[m.surface]
+			elif m.feature.find('SN') > -1 and m.surface.isdigit():
+				number=m.surface
+				# m.next 해서 그 다음 바로 시가 나오는지 체크
+				# 의존성 무시
+			elif m.feature.find('NNBC') > -1 and m.feature.find('시') > -1 and number is not -1:
+				exist_time=True 
+				# 시를 한번 받고 나서 다음에 또 숫자와 시가 안나온 것을 체크하고 end time 가정해서 입력
+				
+
+				end_time = int(number)+1 # delta hour 등
 				if int(time) < 10:
 					time = '0'+str(time)
-				if int(end_time) < 10:
+				if int(end_time) < 10: # 코드 반복
 					end_time = '0'+str(end_time)
+				# 뽑아내는 것은 format 기능으로, c언어 스트링 다루듯이 ( 예외처리 고려 X )
 				time_zone_dict['extract_start']=str(time)+':00'
 				time_zone_dict['extract_end']=str(end_time)+':00'
-			elif (m.surface.find('아침') > -1) or (m.surface.find('브런치') > -1) or (m.surface.find('점심') > -1) or (m.surface.find('저녁') > -1) or (m.surface.find('밤') > -1):
-				time=standard_time_scope[m.surface]
+			
 
 			### Grep purpose
 			elif m.feature.find('CPI') > -1:
-				if total_purpose_count > 1:
-					continue
-
 				parts_of_feature = m.feature.split(',')
+				# part index 지정해서 비교
 				for part in parts_of_feature:
 					if part.find('CPI') > -1:
-						if purpose_count[part] > 0:
-							continue
+						if purpose_count[part] == 0:
+							event_type_list.append({"id":part})
 						purpose_count[part]=purpose_count[part]+1
-						event_type_list.append({"id":part})
-						total_purpose_count=total_purpose_count+1
-						#purposeResult=purposeResult+' '+print_purpose[part]
-
+						
 			### Grep location : google / calyfactorytester3@gmail.com
 			elif (m.feature.find("대학교") > -1):
+				if location_count > 0:
+					m = m.next
+					continue
+
 				if m.surface.find("대학교") > -1:
 					#result=result+m.surface
 					if m.surface in univ_collect_dict:
@@ -131,62 +141,85 @@ def extract_info_from_event(event_hashkey,sentence,start_dt, end_dt, location):
 					else:
 						#print("Can't supported university.")
 						cannot_recommend=True
-
-						
 				else:
 					parts_of_feature = m.feature.split(',')
-					#print(parts_of_feature)
+					# 고정index로 비교
 					for part in parts_of_feature:
 						if part.find('대학교') > 0:
-							#result=result+part
-							
 							if part in univ_collect_dict:
 								location_list.append({'no':location_count,'region':region_collect_dict[univ_collect_dict[part]][0]})
 								location_count = location_count + 1
 							else:
 								#print("Can't supported university.")
 								cannot_recommend=True
-							continue	
-			elif (m.feature.find("지하철") > -1) and (m.surface.find("역") < 0 or m.surface == '동역사'):
+	
+			elif (m.feature.find("지하철") > -1) and (m.surface.find("역") == -1):
+				if location_count > 0:
+					m = m.next
+					continue
+
 				parts_of_feature = m.feature.split(',')
 				for part in parts_of_feature:
 					if part.find('역') > -1:
 						location_list.append({'no':location_count,'region':part})
 						location_count = location_count + 1
-						#result=result+part
-						continue
+						break
+
+
 			elif m.feature.find("지하철") > -1:
+				# is_location boolean, len(location_list)
+				if location_count > 0:
+					m = m.next
+					continue
+				"""
+					continue
+				elif ....:
+					continue
+				"""
 				location_list.append({'no':location_count,'region':m.surface})
 				location_count = location_count + 1
 
 			elif m.feature.find("동이름") > 0:
+				if location_count > 0:
+					m = m.next
+					continue 
+					# logical 하게 안만들었다는 대표케이스. continue는 goto문같은것. 논리적으로 생각하기 싫다. 이런 경우는 그냥 돌려야지
+					
 				#result=result+m.surface
 				if m.surface in region_collect_dict:
 					location_list.append({'no':location_count,'region':region_collect_dict[m.surface][0]})
 					location_count = location_count + 1
 				else:
 					print("Can't supported sub-region.")
-					cannot_recommend=True
+					# 역삼1동 -> 
+					cannot_recommend=True # 회의 후 CANNOT 처리
 			else:
 				#print('else : '+m.surface+'/ '+m.feature)
 				pass
+
 
 			m = m.next
 
 	except RuntimeError as e:
 		print("RuntimeError:", e)
+		# raise exception
+		# https://github.com/CalyFactory/caly/blob/develop/caldavclient/util.py
+		# Except 발생 시, 죽어야되는건지, 값을 보정해줘야되는건지, 예외적인 상황을 반환해야되는건지, 다른 값으로 대체해야되는건지
 
 
-	if exist_time is False:
+	# Docs 참고
+	if exist_time == False:
 		time_zone_dict['extract_start']='None'
 		time_zone_dict['extract_end']='None'
-	if total_purpose_count is 0:
+	if len(event_type_list) < 1:
 		event_type_list="None"
-
-	if location_count is 0:
+	if location_count == 0:
 		location_list="None"
-	if cannot_recommend is True:
+	if cannot_recommend == True:
 		location_list="Cannot"
+
+	# "", '' 왔다갔다?	
+	# string 비교 시 소문자로 비교하는건지, 첫글자만 대문자
 
 	return {
 		'event_hashkey': event_hashkey,
@@ -195,5 +228,4 @@ def extract_info_from_event(event_hashkey,sentence,start_dt, end_dt, location):
 		'event_types':event_type_list
 	}
 
-
-print(extract_info_from_event('hashkey1','test001','2017-07-04 12:00:00','2017-07-04 13:00:00',''))
+# print(extract_info_from_event('-','애드','2017-07-04 12:00:00','2017-07-04 13:00:00','강남역, 대한민국 서울특별시 역삼1동'))
--- extractor/test_extractor.py
+++ extractor/test_extractor.py
@@ -6,19 +6,20 @@ import json
 
 # python3 -m unittest -v testRecommendLocation.py
 
-with open('./common//key/testcase.json') as tcJson:
+with open('../key/testcase.json') as tcJson:
 	testcaseJs = json.load(tcJson)
 
 class TestExtract(unittest.TestCase):
 	# sentence,startDt, endDt
 	def testLocation(self):
-		self.assertTrue(event_extractor.extract_info_from_event('성신여대 미팅','2017-07-04 12:00:00','2017-07-04 13:00:00',''), testcaseJs["101"])
+		self.assertEqual(event_extractor.extract_info_from_event(testcaseJs["N101"]["input"]["event_hashkey"],testcaseJs["N101"]["input"]["summary"],testcaseJs["N101"]["input"]["start_dt"],testcaseJs["N101"]["input"]["end_dt"],testcaseJs["N101"]["input"]["location"]), testcaseJs["N101"]["result"])
+		self.assertEqual(event_extractor.extract_info_from_event(testcaseJs["F001"]["input"]["event_hashkey"],testcaseJs["F001"]["input"]["summary"],testcaseJs["F001"]["input"]["start_dt"],testcaseJs["F001"]["input"]["end_dt"],testcaseJs["F001"]["input"]["location"]), testcaseJs["F001"]["result"])
+		
+	def testPurpose(self):
+		self.assertEqual(event_extractor.extract_info_from_event(testcaseJs["N101"]["input"]["event_hashkey"],testcaseJs["N101"]["input"]["summary"],testcaseJs["N101"]["input"]["start_dt"],testcaseJs["N101"]["input"]["end_dt"],testcaseJs["N101"]["input"]["location"]), testcaseJs["N101"]["result"])
 
-# Testcase 1 : all
-#testAnalysis('3275008b3da8adf4874f6e09cc127c75cf46711b3031cdebd1db9a29')
-# Testcase 2 : without purpose
-#testAnalysis('907d71d0b0809116217205674096ec15929c1dbe5afa9057d98cd439')
-# Testcase 3 : without extractTime
-#recommendLocation.testAnalysis('af0b3b5e551180310106982d9c94786507e397236cf93f345011850f')
-# Testcase 4 : without location
-#testAnalysis('217f53d8b6511daaf659f2911872a72b8be22c39c27714e3e2859f0e')
+	def testTime(self):
+		self.assertEqual(event_extractor.extract_info_from_event(testcaseJs["N101"]["input"]["event_hashkey"],testcaseJs["N101"]["input"]["summary"],testcaseJs["N101"]["input"]["start_dt"],testcaseJs["N101"]["input"]["end_dt"],testcaseJs["N101"]["input"]["location"]), testcaseJs["N101"]["result"])
+
+	def failCase(self):
+		pass
