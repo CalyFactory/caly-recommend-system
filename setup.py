69bd2d12e034e4b985a4e1c5e3a3b309e63bf81b
JSpiner <jspiner@naver.com>
2017-06-14 22:06:22 +0900

    change setup to root

---
+++ setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+
+# extractor 
+setup(
+    name = "extractor",
+    version = "1.0.0",
+    author = "refgjin",
+    author_email = "refgjin@gmail.com",
+    py_modules=[
+        'extractor/event_extractor', 
+        'extractor/common/db_manager'
+    ],
+    description = "caly extractor lib",
+)
+
+# reinforce
+setup(
+    name = "reinforce",
+    version = "1.0.0",
+    author = "reionforce",
+    author_email = "kkk1140@naver.common",
+    py_modules=[
+        'reinforce/reinforce', 
+        'reinforce/common/db_manager',
+        'reinforce/common/mongo_manager',
+        'reinforce/common/util/utils'
+    ],
+    description = "caly reinforce lib",
+)
+
+# reco 
+setup(
+    name = "reco",
+    version = "1.0.0",
+    author = "reco",
+    author_email = "jspiner@naver.com",
+    py_modules=[
+        'reco/reco', 
+        'reco/common/db_manager',
+        'reco/common/mongo_manager',
+        'reco/common/util/utils'
+    ],
+    description = "caly reco lib",
+)
